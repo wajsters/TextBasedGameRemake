73a251f971e9df2097cc5982b23da2fcc6249a74
maxestorr <maxestorr@gmail.com>
2019-06-13 19:00:01 +0100

    Begun attempting to print the character entity in the current room

---
+++ MP-BasicRoomImplementation.py
@@ -0,0 +1,47 @@
+# Prints an ascii room from a .txt file to terminal.
+
+
+def read_room():
+    with open("room.txt", "r") as f:
+        room = f.read().split("\n")  # Create list of rows of tiles in the room
+
+
+
+
+class Entity:
+    """Entity class
+    Arg1: x-coordinate of object (default = 0)
+    Arg2: y-coordinate of object (default = 0)
+
+    Methods:
+    move("direction"): moves the object "up", "right", "down", or "left"
+    
+    """
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+        print(f"Entity instantiated at position {self.coordinates()}")
+
+    def coordinates(self):
+        return (self.x, self.y)
+
+    def move(self, direction):
+        try:
+            if direction.lower() == "up":
+                self.y += 1
+            elif direction.lower() == "right":
+                self.x += 1
+            elif direction.lower() == "down":
+                self.y -= 1
+            elif direction.lower() == "left":
+                self.x -= 1
+            else:
+                print("Warning: directional input only works for the four",
+                      "cardinal directions\n(up, right, down, or left).\n")
+
+            print(f"Entity position updated to {self.coordinates()}")
+        except AttributeError:
+            print("Error: the move() method in Entity class requires string",
+                  "input.\n")
+
--- print_room.py
+++
@@ -1,7 +0,0 @@
-# Prints an ascii room from a .txt file to terminal.
-
-
-def print_room():
-    with open("room.txt", "r") as f:
-        room = f.read()
-
